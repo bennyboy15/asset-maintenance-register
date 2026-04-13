11d384c60f7d2c8d6aabfe29fbae0f099c57aee9
Benjamin Harvey <benjaminharvey2000@gmail.com>
2026-04-13 18:33:28 +0930

    Global package scripts to run frontend / backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import SuppliersPage from './pages/SuppliersPage'
+import AssetsPage from './pages/AssetsPage'
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/assets" element={<AssetsPage />} />
       <Route path="/suppliers" element={<SuppliersPage />} />
     </Routes>
   )
